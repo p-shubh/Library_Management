428f0d2c20eabeeace006ca0f2f08c099d8dc8cb
shubham prajapati <shubhamprajapati032@gmail.com>
2022-07-16 15:48:40 +0530

    append work pending

--- studentsOrderReq.go
+++ studentsOrderReq.go
@@ -1,19 +1,14 @@
 package main
 
 import (
+	"fmt"
+	"log"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
 
 func studentsOrderReq(c *gin.Context) {
-
-	// if 1 == 1 {
-	// 	res := gin.H{
-	// 		"result": "true",
-	// 	}
-	// 	c.JSON(http.StatusOK, res)
-	// }
 
 	reqBody := OrderRequested{}
 
@@ -24,11 +19,9 @@ func studentsOrderReq(c *gin.Context) {
 
 	row := DB.QueryRow(sqlStatatement)
 
-	// fmt.Println("check getUserByEmail", reqBody)
-
 	err := row.Scan(&reqBody.Id, &reqBody.Book_id, &reqBody.Issue_date, &reqBody.Approve_grant, &reqBody.Order_ID)
 
-	// fmt.Println("check getUserByEmail", reqBody)
+	fmt.Println("reqBody.Order_ID of pending : ", reqBody.Order_ID)
 
 	if err != nil {
 		//log.Fatal("ppt", err)
@@ -52,31 +45,78 @@ func studentsOrderReq(c *gin.Context) {
 
 	// return
 
-	sqlStatatement2 := `SELECT "order_id" FROM "public"."students_order_detail" where "approve_grant" = 'approved';`
+	// sqlStatatement2 := `SELECT "order_id" FROM "public"."students_order_detail" where "approve_grant" = 'approved';`
 
-	row2 := DB.QueryRow(sqlStatatement2)
+	// row2 := DB.QueryRow(sqlStatatement2)
 
-	// fmt.Println("check getUserByEmail", reqBody)
+	// // fmt.Println("check getUserByEmail", reqBody)
 
-	err2 := row2.Scan(&reqBody.Order_ID)
+	// err2 := row2.Scan(&reqBody.Order_ID)
 
-	// fmt.Println("check getUserByEmail", reqBody)
+	// fmt.Println("reqBody.Order_ID", reqBody.Order_ID)
 
-	if err2 != nil {
-		// log.Fatal(err2)
-		res := gin.H{
-			"still pending order id": reqBody.Order_ID,
-			"error":                  "no any approved order list",
+	grant_status := "approved"
+
+	total_rows := count_rows(grant_status)
+
+	for i := total_rows; i >= 1; i-- {
+
+		sqlStatatement2 := `SELECT "order_id" FROM "public"."students_order_detail" where "approve_grant" = 'approved';`
+
+		row2 := DB.QueryRow(sqlStatatement2)
+
+		err2 := row2.Scan(&reqBody.Order_ID)
+
+		fmt.Println("reqBody.Order_ID", reqBody.Order_ID)
+		if err2 != nil {
+			// log.Fatal(err2)
+			res := gin.H{
+				"still pending order id": reqBody.Order_ID,
+				"error":                  "no any approved order list",
+			}
+			c.JSON(http.StatusOK, res)
+		} else {
+			res := gin.H{
+				"status":                 "success",
+				"approved order_id list": reqBody.Order_ID,
+			}
+			c.JSON(http.StatusOK, res)
 		}
-		c.JSON(http.StatusOK, res)
-	} else {
-		res := gin.H{
-			"status":                 "success",
-			"approved order_id list": reqBody.Order_ID,
-		}
-		c.JSON(http.StatusOK, res)
+
 	}
+
+	var all_order_id []int
+
+	all_order_id = append(all_order_id, reqBody.Order_ID)
+
+	// if err2 != nil {
+	// 	// log.Fatal(err2)
+	// 	res := gin.H{
+	// 		"still pending order id": all_order_id,
+	// 		"error":                  "no any approved order list",
+	// 	}
+	// 	c.JSON(http.StatusOK, res)
+	// } else {
+	res := gin.H{
+		"status":                 "success",
+		"approved order_id list": all_order_id,
+	}
+	c.JSON(http.StatusOK, res)
+	// }
 
 	// return
 
+}
+
+func count_rows(grant_status string) int {
+	var count int
+
+	err := DB.QueryRow("SELECT COUNT(*) FROM students_order_detail where approve_grant = $1", grant_status).Scan(&count)
+	switch {
+	case err != nil:
+		log.Fatal(err)
+	default:
+		fmt.Println("Number of rows are", count)
+	}
+	return count
 }
